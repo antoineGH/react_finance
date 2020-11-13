ced521366bd6e949bd485e103133d1980281354c
Templars <antoine.ratat@gmail.com>
2020-11-14 03:22:52 +0800

    Historical Exchange Rate Page State

--- src/views/examples/FinanceFeed.js
+++ src/views/examples/FinanceFeed.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import UserHeader from 'components/Headers/UserHeader.js'
+import fetchNewsFeed from '../currency/utils/fetchNewsFeed'
 
-export default function FinanceFeed() {
-	const welcome = 'Finance Feed'
-	const message = 'Our currency converter calculator will convert your money based on current values from around the world.'
-	const background = {
-		color: 'linear-gradient(to right, #141e30, #243b55)',
+export default class FinanceFeed extends Component {
+	// --- CLASS CONSTRUCTOR ---
+	constructor(props) {
+		super(props)
+		this.state = {
+			newsFeedLoaded: false,
+			newsFeedError: false,
+			newsFeed: [],
+		}
 	}
-	return (
-		<div>
-			<UserHeader welcome={welcome} message={message} background={background} />
-			<p>FinanceFeed</p>
-		</div>
-	)
+
+	// --- COMPONENT LIFECYCLE ---
+	componentDidMount() {
+		this.getNewsFeed()
+	}
+
+	// --- CLASS METHODS ---
+	getNewsFeed(interests) {
+		this.setState({ newsFeedError: false, newsFeedLoaded: false })
+		if (!interests) {
+			interests = ['Apple', 'Tesla', 'Microsoft']
+			fetchNewsFeed('cityfalcon', interests)
+				.then((response) => {
+					const stories = response.stories
+					this.setState({ newsFeedError: false, newsFeedLoaded: true, newsFeed: stories })
+				})
+				.catch((error) => {
+					this.setState({ newsFeedError: true })
+				})
+			return
+		}
+	}
+
+	render() {
+		const welcome = 'Finance Feed'
+		const message = 'Our currency converter calculator will convert your money based on current values from around the world.'
+
+		return (
+			<div>
+				<UserHeader welcome={welcome} message={message} />
+				<p>FinanceFeed</p>
+			</div>
+		)
+	}
 }
--- src/views/examples/HistoricalRate.js
+++ src/views/examples/HistoricalRate.js
@@ -1,16 +1,196 @@
-import React from 'react'
+import React, { Component } from 'react'
 import UserHeader from 'components/Headers/UserHeader.js'
+import getDate from '../currency/utils/getDate'
+import getDateBefore from '../currency/utils/getDateBefore'
+import sortDate from '../currency/utils/sortDate'
+import fetchCurrency from 'views/currency/utils/fetchCurrency'
+import fetchHistoryCurrency from '../currency/utils/fetchHistoryCurrency'
+import Pagination from 'react-js-pagination'
+import { authFetch } from '../../auth'
+import Select from 'react-dropdown-select'
+import { currenciesName } from '../currency/utils/currenciesName'
+import BarLoader from 'react-spinners/BarLoader'
+import { Table } from 'reactstrap'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import { Card, CardHeader, CardBody, FormGroup, Input, Container, Row } from 'reactstrap'
 
-export default function HistoricalRate() {
-	const welcome = 'Historical Exchange Rate'
-	const message = 'Foreign Exchange Rates Historical Search.'
-	const background = {
-		color: 'linear-gradient(to right, #141e30, #243b55)',
+export default class HistoricalRate extends Component {
+	// --- CLASS CONSTRUCTOR ---
+	constructor(props) {
+		super(props)
+		this.state = {
+			listCurrency: [],
+			listCurrencyError: false,
+			listCurrencyLoaded: false,
+			listCurrencyHistory: [],
+			listCurrencyHistoryError: false,
+			listCurrencyHistoryLoaded: false,
+			selectedCurrency: 'Default Currency',
+			filterMethod: 'destAsc',
+			search: '',
+		}
 	}
-	return (
-		<div>
-			<UserHeader welcome={welcome} message={message} background={background} />
-			<p>HistoricalRate</p>
-		</div>
-	)
+	// --- COMPONENT LIFECYCLE ---
+
+	componentDidMount() {
+		this.fetchUserSettings().then((response) => {
+			this.setState({ selectedCurrency: response.default_currency })
+			fetchCurrency(response.default_currency)
+				.then((response) => {
+					const listCurrency = []
+					for (const [prop, value] of Object.entries(response.rates)) {
+						const currencyName = '(' + currenciesName[prop] + ')'
+						listCurrency.push({
+							value: prop,
+							label: `${prop} ${currencyName}`,
+							rate: value,
+						})
+					}
+					const date = new Date(Date.now())
+					const start_date = getDate(date)
+					const end_date = getDateBefore(date, 1, 'months')
+					this.getListExchange(start_date, end_date, this.state.selectedCurrency, listCurrency)
+					this.setState({
+						listCurrency: listCurrency,
+						listCurrencyError: false,
+						listCurrencyLoaded: true,
+						listCurrencyHistoryError: false,
+						listCurrencyHistoryLoaded: true,
+					})
+				})
+				.catch((error) => {
+					console.log(error)
+					this.setState({ listCurrencyError: true, listCurrencyHistoryError: true })
+				})
+		})
+	}
+
+	// --- CLASS METHODS ---
+
+	async fetchUserSettings() {
+		const response = await authFetch('http://localhost:5000/api/user/setting', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+		let responseJson = undefined
+		let errorJson = undefined
+
+		if (response.ok) {
+			responseJson = await response.json()
+		} else {
+			if (response.status === 400) {
+				errorJson = await response.json()
+			}
+			if (response.status === 401) {
+				errorJson = await response.json()
+			}
+		}
+		return new Promise((resolve, reject) => {
+			responseJson ? resolve(responseJson) : reject(errorJson.message)
+		})
+	}
+
+	getListExchange(startDate, endDate, baseCurrency, listCurrency) {
+		this.setState({ listCurrencyHistoryError: false, listCurrencyHistoryLoaded: false })
+		const items = 33
+		for (let i = 0; i < items - 1; i++) {
+			const destCurrency = listCurrency[i]['value']
+			if (destCurrency === baseCurrency) continue
+			fetchHistoryCurrency(endDate, startDate, baseCurrency, destCurrency)
+				.then((response) => {
+					const orderedDates = sortDate(response)
+					const historyPercentage = this.getHistoryPercentage(orderedDates, destCurrency)
+					const keyEndDate = Object.keys(orderedDates)[orderedDates.length]
+					const rate = orderedDates[keyEndDate][destCurrency]
+
+					this.setState({
+						listCurrencyHistory: [
+							...this.state.listCurrencyHistory,
+							{
+								baseCurrency: baseCurrency,
+								destCurrency: destCurrency,
+								rate: rate,
+								historyPercentage: historyPercentage,
+							},
+						],
+					})
+					if (this.state.listCurrencyHistory.length === items - 2) {
+						this.setState({ listCurrencyLoaded: true })
+					}
+				})
+				.catch((error) => {
+					this.setState({ listCurrencyError: true })
+				})
+		}
+	}
+
+	getHistoryPercentage(orderedDates, destCurrency) {
+		const orderedDatesKeys = Object.keys(orderedDates)
+		const firstKey = orderedDatesKeys[0]
+		const lastKey = orderedDatesKeys[orderedDatesKeys.length - 1]
+		const t0 = orderedDates[firstKey][destCurrency]
+		const t1 = orderedDates[lastKey][destCurrency]
+
+		// Formula Historical Evolution % = ((t1 - t0) / t0) * 100
+		let historyPercentage = ((t1 - t0) / t0) * 100
+		historyPercentage = Math.round(historyPercentage * 10000) / 10000
+		return historyPercentage
+	}
+
+	handleChange(selected) {
+		selected && this.setState({ selectedCurrency: selected[0].value })
+		const date = new Date(Date.now())
+		const start_date = getDate(date)
+		const end_date = getDateBefore(date, 1, 'months')
+		this.setState({ listCurrencyHistory: [] })
+		this.getListExchange(start_date, end_date, selected[0].value, this.state.listCurrency)
+	}
+
+	render() {
+		const { listCurrency, listCurrencyLoaded, listCurrencyError, selectedCurrency } = this.state
+		const welcome = 'Historical Exchange Rate'
+		const message = 'Foreign Exchange Rates Historical Search.'
+
+		return (
+			<div>
+				<UserHeader welcome={welcome} message={message} />
+				<br />
+				{/* User settings */}
+				<h6 className='heading-small text-muted mb-4 ml-4'>User settings</h6>
+				<div className='pl-lg-4'>
+					<Row>
+						<Col lg='3'>
+							<FormGroup>
+								<label className='form-control-label' htmlFor='input-username'>
+									Select Currency
+								</label>
+								<Select
+									key={new Date().getTime()}
+									options={listCurrency}
+									values={[
+										{
+											label: selectedCurrency + ' (' + currenciesName[selectedCurrency] + ')',
+											value: selectedCurrency,
+										},
+									]}
+									onChange={(selected) => this.handleChange(selected)}
+									keepSelectedInList={true}
+									dropdownHandle={true}
+									closeOnSelect={true}
+									clearable={false}
+									loading={listCurrencyLoaded ? false : true}
+									disabled={listCurrencyError ? true : false}
+									style={{ borderRadius: '.25rem' }}
+								/>
+							</FormGroup>
+						</Col>
+					</Row>
+				</div>
+			</div>
+		)
+	}
 }
